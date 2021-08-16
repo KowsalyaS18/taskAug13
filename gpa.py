b8fa76688b7f9ee9b41dfc9c5210d44f9cbcb12f
KowsalyaS18 <87513120+KowsalyaS18@users.noreply.github.com>
2021-08-16 09:23:57 +0530

    Create gpa.py
    Create a csv file with some data. Read csv using pandas
    Create dataframe from a dict and save it in a csv
    Calculate percentage of missing values in csv file(create csv file with some missing values) 
    Replace missing values with “N/A” text using pandas
    Create student data csv file. (roll_no, student_name, year, 1st sem_gpa, …… 8 sem gpa)
    Read this student_data file, find the highest gpa scored by the student and save it in a new column, “highest_score”, “highest_scored_sem”. And save it in a new file

---
+++ gpa.py
@@ -0,0 +1,61 @@
+import json
+import flask
+from flask import request,jsonify
+import pandas as pd
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['POST'])
+def api():
+    ip=request.get_json()
+    print(ip)
+    df=get_working_df()
+    res = filter(df,ip)
+    result = res.to_json(orient="records")
+    parsed = json.loads(result)
+    return jsonify(parsed)
+
+
+def get_working_df():
+    df = pd.read_csv('data.csv')
+    return df
+
+
+def filter(df,ip):
+    for key, val in ip.items():
+        if len(val) != 0:
+            df = (df[(df[key] == val[0])])
+    return df
+
+
+@app.route('/missing', methods=['GET'])
+def missing_val():
+    df = pd.read_csv('data.csv')
+    percent_missing = df.isnull().sum() * 100 / len(df)
+    return percent_missing.to_dict()
+
+
+@app.route('/nul', methods=['GET'])
+def fill_null_val():
+    df = pd.read_csv('data.csv')
+    res=df.fillna("N/A")
+    result=res.to_json(orient="records")
+    parsed = json.loads(result)
+    return jsonify(parsed)
+
+@app.route('/gpa', methods=['GET'])
+def student_filer():
+    data = pd.read_csv('student.csv')
+    data['max_gpa'] = data[
+        ['gpa_sem1', 'gpa_sem2', 'gpa_sem3', 'gpa_sem4', 'gpa_sem5', 'gpa_sem6', 'gpa_sem7', 'gpa_sem8']].max(axis=1)
+    data['max_sem'] = data[
+        ['gpa_sem1', 'gpa_sem2', 'gpa_sem3', 'gpa_sem4', 'gpa_sem5', 'gpa_sem6', 'gpa_sem7', 'gpa_sem8']].idxmax(axis=1)
+    result=data.to_json(orient="records")
+    parsed=json.loads(result)
+    return jsonify(parsed)
+
+if __name__ == '__main__':
+    app.run(debug=True)
